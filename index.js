7cbe6d7707f742091b564d07b458d0355ed236dd
Subodh Aryal <43497160+asubodh@users.noreply.github.com>
2020-07-01 14:25:31 +0545

    Some Random Changes!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const discord = require ('discord.js');
 
 var client = new discord.Client();
 const ms = require('ms');
-const token = "NzI3Njk4NTg0MjQxNDM4ODIx.XvvpXQ.rdxXwlrjx10N_zFgiwG0M52M2ZY"
+const token = ""
 const emojiChannelID = '727510204635283487';
 
 const prefix = "+";
@@ -25,22 +25,23 @@ client.on("message",(message) => {
     
     if (msg.startsWith (prefix + "hello")) {
         message.reply ('Hi!');
+        console.log(`User ID: ${message.author} + Message: ${message}`);
         
     }
 
 
-////POLL +pollyesno
-if (msg.includes (prefix + "pollyesno")) {
+////POLL +poll
+if (msg.includes (prefix + "poll")) {
     let args=message.content.substring(prefix.length).split(" ");
     if (!message.member.hasPermission("ADMINISTRATOR")) return;
     switch(args[0]){
 
-        case "pollyesno":
+        case "poll":
             const Embed = new discord.MessageEmbed()
             .setColor(0xFFC300)
             .setTitle("Easy Poll")
             .setDescription("Wrong Command Include Your Message!");
-
+            console.log(`User ID: ${message.author}  Message: ${message}`);
             if(!args[1]){
                 message.channel.send(Embed);
                 break;
@@ -57,33 +58,5 @@ if (msg.includes (prefix + "pollyesno")) {
         break;
   }  }
 });
-
-////POLL +pollthreeormore
-if (msg.includes (prefix + "poll")) {
-    let args=message.content.substring(prefix.length).split(" ");
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    switch(args[0]){
-
-        case "poll":
-            const Embed = new discord.MessageEmbed()
-            .setColor(0xFFC300)
-            .setTitle("Easy Poll")
-            .setDescription("Wrong Command Include Your Message!");
-
-            if(!args[1]){
-                message.channel.send(Embed);
-                break;
-            }
-
-            let msgArgs = args.slice(1).join(" ");
-
-            message.channel.send("📋 " + "**" + msgArgs + "**").then(messageRecation => {
-                messageRecation.react("👍");
-                messageRecation.react("👎");
-                message.delete();
-            });
-
-        break;
-  }  };
 
 client.login (token);
